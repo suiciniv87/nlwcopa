b73a537ebda94ed1dcf8ee7b7bd8a64d4306ac41
Vinícius <vikosk8@hotmail.com>
2022-11-02 15:02:39 -0300

    part three

--- mobile/nlwcopamobile/App.tsx
+++ mobile/nlwcopamobile/App.tsx
@@ -2,19 +2,23 @@ import { NativeBaseProvider, StatusBar } from 'native-base';
 import { useFonts, Roboto_400Regular, Roboto_500Medium, Roboto_700Bold } from '@expo-google-fonts/roboto';
 
 import { SignIn } from './src/screens/Signin';
+import { Pools } from './src/screens/Pools';
 import { Loading } from './src/components/Loading';
 
 import {THEME} from './src/styles/theme';
+import { AuthContextProvider } from './src/contexts/AuthContext';
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_500Medium, Roboto_700Bold });
 
   return (
     <NativeBaseProvider theme={THEME}>
-      <StatusBar barStyle="light-content" backgroundColor="transparent" translucent />
-      {
-        fontsLoaded ? <SignIn /> : <Loading /> 
-      }
+      <AuthContextProvider>
+        <StatusBar barStyle="light-content" backgroundColor="transparent" translucent />
+        {
+          fontsLoaded ? <Pools /> : <Loading /> 
+        }
+      </AuthContextProvider>
     </NativeBaseProvider>
   );
 }
